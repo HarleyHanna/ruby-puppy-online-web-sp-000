04b16a47afc2f47262dbab1fc115d9c430cdfd72
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-23 13:40:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |name|
+      puts name
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  
+  
+    
+  
+end
